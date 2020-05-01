5f6a98497e47f8da4eeb461f7d7aca42a68c7981
sushant3kumar <64606670+sushant3kumar@users.noreply.github.com>
2020-05-01 19:19:32 +0530

    Create SingleNumbers.java

---
+++ Leetcode/SingleNumbers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        if(nums.length>1)
+        {
+            
+        
+            for(int i=0;i<nums.length-1;)
+            {
+                if(nums[i]!=nums[i+1])
+                {
+                    return(nums[i]);
+                }
+                i=i+2;
+            }
+            if(nums[nums.length-1]!=nums[nums.length-2])
+            {
+                return(nums[nums.length-1]);
+            }
+           
+        
+        }
+        return(nums[0]);
+    }
+}
